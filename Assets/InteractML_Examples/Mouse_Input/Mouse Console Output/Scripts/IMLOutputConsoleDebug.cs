7cabe9a4f515517e3a7e64fafcdbebb80e5b0b7e
Carlos <cgonz011@gold.ac.uk>
2019-12-15 17:04:46 +0000

    Cleaned up Data from Controllers + Arranged mouse demos nicely in the folder structure

---
+++ Assets/InteractML_Examples/Mouse_Input/Mouse Console Output/Scripts/IMLOutputConsoleDebug.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InteractML;
+
+public class IMLOutputConsoleDebug : MonoBehaviour
+{
+    /// <summary>
+    /// IML Component to read data from
+    /// </summary>
+    public IMLComponent MLComponent;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // Only read data if ML component is assigned
+        if (MLComponent != null)
+        {
+            // Only read data if there any outputs to read
+            if (MLComponent.IMLControllerOutputs.Count > 0)
+            {
+                // Show each output on console
+                foreach (var output in MLComponent.IMLControllerOutputs)
+                {
+                    string resultToDebug = "Outputs are: \n";
+                    for (int i = 0; i < output.Length; i++)
+                    {
+                        // Add a line break at the end in case we have more than one output
+                        resultToDebug += output[i].ToString() + "\n";
+                    }
+                    // Print on console
+                    Debug.Log(resultToDebug);
+                }
+            }
+        }
+    }
+}
