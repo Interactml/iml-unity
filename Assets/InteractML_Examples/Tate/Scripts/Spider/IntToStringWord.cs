1286b212caca9e9d227f727085ed88f82d8a6c0a
ClariceHilton <claricemayhilton@gmail.com>
2020-03-01 17:56:40 +0000

    Created IML Controller for spider (unfinished)
    Created an IML Controller that will handle 3 gestures.
    Created 4 training examples sets for each gesture + idle
    Unfinished creating examples for gestures 2 and 3

--- Assets/InteractML_Examples/Tate/Scripts/Spider/IntToStringWord.cs
+++ Assets/InteractML_Examples/Tate/Scripts/Spider/IntToStringWord.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 /// <summary>
 /// Struct that holds the equivalency between an int and a string
 /// </summary>
+[System.Serializable]
 public struct IntToStringWord 
 {
     public int ExpectedInteger;
