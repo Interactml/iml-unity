f7b54433513aeab87b0bde3e08e9752ab575c97c
Carlotes_Laptop <carlotes247@gmail.com>
2020-04-20 21:15:09 +0100

    Updated IML Config node Editor so that dynamic output nodes are only drawn where the data types are

--- Assets/InteractML/Scripts/IML Nodes/Editor/IMLConfigurationEditor.cs
+++ Assets/InteractML/Scripts/IML Nodes/Editor/IMLConfigurationEditor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using ReusableMethods;
 using System;
+using System.Linq;
 #if UNITY_EDITOR
 using UnityEditor;
 using XNodeEditor;
@@ -34,8 +35,10 @@ namespace InteractML
             // Override label width for this node (since some names are quite long)
             LabelWidth = 184;
 
-            base.OnBodyGUI();
+            // DEFAULT DRAWING EXCEPT NO DYNAMIC PORTS
+            DefaultOnBodyGUINoDynamicPorts();
 
+            // Get reference to imlConfig node
             m_IMLConfigNode = (target as IMLConfiguration);
 
             // SHOW KEYBOARD SHORTCUTS
@@ -115,6 +118,68 @@ namespace InteractML
         #endregion
 
         #region Methods
+
+        private void DefaultOnBodyGUINoDynamicPorts()
+        {
+#if ODIN_INSPECTOR
+            inNodeEditor = true;
+#endif
+
+            // Unity specifically requires this to save/update any serial object.
+            // serializedObject.Update(); must go at the start of an inspector gui, and
+            // serializedObject.ApplyModifiedProperties(); goes at the end.
+            serializedObject.Update();
+            string[] excludes = { "m_Script", "graph", "position", "ports" };
+
+#if ODIN_INSPECTOR
+            InspectorUtilities.BeginDrawPropertyTree(objectTree, true);
+            GUIHelper.PushLabelWidth(LabelWidth);
+            objectTree.Draw(true);
+            InspectorUtilities.EndDrawPropertyTree(objectTree);
+            GUIHelper.PopLabelWidth();
+#else
+
+            // Iterate through serialized properties and draw them like the Inspector (But with ports)
+            SerializedProperty iterator = serializedObject.GetIterator();
+            bool enterChildren = true;
+            EditorGUIUtility.labelWidth = LabelWidth;
+            while (iterator.NextVisible(enterChildren))
+            {
+                enterChildren = false;
+                if (excludes.Contains(iterator.name)) continue;
+                NodeEditorGUILayout.PropertyField(iterator, true);
+            }
+#endif
+
+            // [DISABLED]Iterate through dynamic ports and draw them in the order in which they are serialized
+            //foreach (XNode.NodePort dynamicPort in target.DynamicPorts)
+            //{
+            //    if (NodeEditorGUILayout.IsDynamicPortListPort(dynamicPort)) continue;
+            //    NodeEditorGUILayout.PortField(dynamicPort);
+            //}
+
+            serializedObject.ApplyModifiedProperties();
+
+#if ODIN_INSPECTOR
+            // Call repaint so that the graph window elements respond properly to layout changes coming from Odin    
+            if (GUIHelper.RepaintRequested) {
+                GUIHelper.ClearRepaintRequest();
+                window.Repaint();
+            }
+#endif
+
+#if ODIN_INSPECTOR
+            inNodeEditor = false;
+#endif
+
+            // CODE TO CHANGE NODE COLOR [ONLY DEV MODE]
+            if (target.DevMode)
+            {
+                customNodeColor = target.CustomColor;
+                customNodeColor = EditorGUILayout.ColorField(customNodeColor);
+                target.CustomColor = customNodeColor;
+            }
+        }
 
         private void ShowExpectedInputsConfigLogic()
         {
