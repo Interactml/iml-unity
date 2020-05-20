dd60db29b3614bcae87787053c692e7d6f4beabd
claricehilton <claricemayhilton@gmail.com>
2020-05-20 10:33:03 +0100

    Desearialization of json for tooltips. IMLTooltips DataType

---
+++ Assets/InteractML/Scripts/IML Nodes/Tooltips/DataTypes/IMLNodeTooltipArray.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+
+namespace InteractML
+{
+  
+    [System.Serializable]
+    public struct IMLNodeTooltipArray {
+        public IMLNodeTooltips[] TooltipArray;
+    }
+}
---
+++ Assets/InteractML/Scripts/IML Nodes/Tooltips/Scripts/IMLTooltipsSerialization.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.IO;
+using Newtonsoft.Json;
+using System;
+
+namespace InteractML
+{
+    public class IMLTooltipsSerialization : MonoBehaviour
+    {
+        private static string m_FolderPath = "InteractML/Data/Tooltips/";
+        private static string m_DataPath;
+        private static string m_FileExtension = ".json";
+
+        /// <summary>
+        /// Loads Tooltip JSON 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns>Returns a IMLNodeTooltips object </returns>
+        public static IMLNodeTooltips LoadTooltip(string fileName)
+        {
+            string jsonFile = File.ReadAllText(SetUpFileNamesAndPaths(fileName));
+            IMLNodeTooltips toolTips = JsonConvert.DeserializeObject<IMLNodeTooltips>(jsonFile);
+            return toolTips;
+        }
+
+        /// <summary>
+        /// Sets up file path for tooltip
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns>Returns file path for tooltip json</returns>
+        private static string SetUpFileNamesAndPaths(string fileName)
+        {
+            // Set up data path (Application.dataPath + FolderName + FileName + FileExtension)
+            m_DataPath = Path.Combine(Application.dataPath, m_FolderPath + fileName + m_FileExtension);
+
+            return m_DataPath;
+
+        }
+    }
+}
