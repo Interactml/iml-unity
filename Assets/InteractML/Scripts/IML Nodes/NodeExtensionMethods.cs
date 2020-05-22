c10a39a964989c075260bcb96cbbdf6e1c3255db
Carlotes_Laptop <carlotes247@gmail.com>
2020-05-22 20:25:57 +0100

    Disconnect connection method
            /// Disconnects two nodes if types don't match
            /// <returns>True if disconnected. False otherwise</returns>
            public static bool DisconnectIfNotType<T, U>(this Node node, NodePort from, NodePort to)

--- Assets/InteractML/Scripts/IML Nodes/NodeExtensionMethods.cs
+++ Assets/InteractML/Scripts/IML Nodes/NodeExtensionMethods.cs
@@ -35,5 +35,31 @@ namespace InteractML
                 return null;
             }
         }
+
+        /// <summary>
+        /// Disconnects two nodes if types don't match
+        /// </summary>
+        /// <typeparam name="T">Type from</typeparam>
+        /// <typeparam name="U">Type to</typeparam>
+        /// <param name="node"></param>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <returns>True if disconnected. False otherwise</returns>
+        public static bool DisconnectIfNotType<T, U>(this Node node, NodePort from, NodePort to)
+        {
+            // Check if types are matching
+            bool isValidType = false;
+            if (to.node is T)
+            {
+                isValidType = from.node is U;
+                if (!isValidType)
+                {
+                    from.Disconnect(to);
+                }
+            }
+
+            //True if disconnected.False otherwise
+            return !isValidType;
+        }
     }
 }
