9286b73e740f1af9c80516f50c81e19593d22ef1
Carlotes_Laptop <carlotes247@gmail.com>
2020-06-16 17:34:53 +0100

    Simple VR demo controlling light

---
+++ Assets/InteractML_Examples/VR_Interface/Scripts/LightController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InteractML;
+
+public class LightController : MonoBehaviour
+{
+    public Light light;
+
+    [PullFromIMLController]
+    public Vector3 SetLightRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (light != null)
+        {
+            var rotation = light.transform.rotation;
+            rotation.eulerAngles = SetLightRotation;
+            light.transform.rotation = rotation;
+        }
+    }
+}
