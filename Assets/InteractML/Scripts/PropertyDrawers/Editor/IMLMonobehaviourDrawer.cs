b59b1f1c50131cd712e3539562b0baf6000dce74
Carlotes_Laptop <carlotes247@gmail.com>
2020-06-23 12:21:25 +0100

    Fix custom property drawer IMLMonobehaviour showing wrong info

--- Assets/InteractML/Scripts/PropertyDrawers/Editor/IMLMonobehaviourDrawer.cs
+++ Assets/InteractML/Scripts/PropertyDrawers/Editor/IMLMonobehaviourDrawer.cs
@@ -6,6 +6,7 @@ using InteractML;
 public class IMLMonobehaviourDrawer : PropertyDrawer
 {
     Texture2D m_ScriptTexture;
+    Texture2D m_GOTexture;
     bool m_TextureLoaded = false;
 
     // Draw the property inside the given rect
@@ -18,37 +19,50 @@ public class IMLMonobehaviourDrawer : PropertyDrawer
         EditorGUI.BeginProperty(position, label, property);
         
         // Load texture once
-        if (!m_TextureLoaded || m_ScriptTexture == null)
+        if (!m_TextureLoaded)
         {
             // Load texture
             m_ScriptTexture = EditorGUIUtility.FindTexture("cs Script Icon");
+            m_GOTexture = EditorGUIUtility.FindTexture("Prefab Icon");
             m_TextureLoaded = true;
         }
 
         // Draw label
         // Attempt to get name of script class
-        Object SerializedScript = property.objectReferenceValue;
+        Object serializedScript = property.objectReferenceValue;
         string scriptClassName = null;
-        if (SerializedScript != null)
-            scriptClassName = SerializedScript.name;
+        string goClassName = null;
+        if (serializedScript != null)
+        {
+            scriptClassName = serializedScript.GetType().ToString();
+            goClassName = serializedScript.name;
+        }
         else
+        {
             scriptClassName = "Null";
+            goClassName = "Null";
+        }
+
         // Assign extra text to label
-        label.text = string.Concat(label.text, ": ", scriptClassName);
+        label.text = string.Concat("Script: ", scriptClassName);
         // Assign icon to label
         label.image = m_ScriptTexture;
-
-        // Draw label with extra info and label
+        // Increase original position height to account for extra drawing
+        position.y = position.y + 20;
+        position.height = position.height + 10;
+        // Draw label for script
         EditorGUI.LabelField(position, label);
 
-        //position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
-
-
-        // Draw icon 
-        //EditorGUI.DrawTextureTransparent(position, m_ScriptTexture);
-        //EditorGUI.PropertyField(position, property, GUIContent.none);
+        // Change text and icon for gameobject
+        label.text = string.Concat("GameObject: ", goClassName);
+        label.image = m_GOTexture;
+        // Decrease original position height to account for extra drawing
+        position.y = position.y - 20;
+        // Draw label for gameObject
+        EditorGUI.LabelField(position, label);
 
         EditorGUI.EndProperty();
+
 
     }
 }
