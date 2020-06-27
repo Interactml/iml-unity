72feeff102940cdd9205e92d65f96957eaea80c8
Carlotes_Laptop <carlotes247@gmail.com>
2020-06-27 18:02:39 +0100

    addded colorChangerHSV script with fix on H

---
+++ Assets/InteractML_Examples/Mouse_Input/Mouse Colour Change/Scripts/colorChangerHSV.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InteractML;
+
+/// <summary>
+/// Example script to control the colour of a plane with regression
+/// </summary>
+public class colorChangerHSV : MonoBehaviour
+{
+    Renderer renderer;
+    private Color regressionColor;
+    // IML values regression
+    [PullFromIMLController]
+    public float H, S, V;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>(); // grab the renderer component
+        if (regressionColor == null)
+            new Color();
+        S = 0.5f;
+        V = 0.5f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        H = (H / 360);
+        if (H > 1)
+        {
+            H = 1;
+        }
+        else if (H < 0)
+        {
+            H = 0;
+        }
+        // Get the values from the iml controller
+        regressionColor = Color.HSVToRGB(H, S, V);
+        renderer.material.SetColor("_Color", regressionColor);
+    }
+}
