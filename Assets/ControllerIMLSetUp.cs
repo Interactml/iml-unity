f6a5f472f4cb5fc3d338e14decbb115305fe6bf9
claricehilton <claricemayhilton@gmail.com>
2020-10-09 10:34:20 +0100

    steam set up ad scene

---
+++ Assets/ControllerIMLSetUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ControllerIMLSetUp : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType; // 1
+    public SteamVR_Action_Boolean teleportAction; // 2
+    public SteamVR_Action_Boolean grabAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SteamVR_Actions.default_GrabPinch.AddOnStateDownListener(, SteamVR_Input_Sources.Any);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
