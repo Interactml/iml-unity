704a2cf8a6a6554706f6aad531ecbf9e3ca5f101
claricehilton <claricemayhilton@gmail.com>
2020-12-01 10:06:13 +0000

    uncomment porttip code

--- Assets/InteractML/Scripts/IML Nodes/Editor/IMLNodeEditor.cs
+++ Assets/InteractML/Scripts/IML Nodes/Editor/IMLNodeEditor.cs
@@ -763,8 +763,8 @@ namespace InteractML
                     showporthelper = true;
                     for (int i = 0; i < ports.Count; i++)
                     {
-                        // must come back here to fix - pbroken with new init
-                        /*if (window.hoveredPort == ports[i] && portTips.Length != 0 && portTips.Length > 0)
+                        
+                        if (window.hoveredPort == ports[i] && portTips.Length != 0 && portTips.Length > 0)
                         {
                             // If there are more ports than tooltips show the last one in the list (asthe number of outputs in a mls node is undeterminanate)
                             if (i >= portTips.Length)
@@ -776,7 +776,7 @@ namespace InteractML
                                 TooltipText = portTips[i];
                             }
 
-                        }*/
+                        }
 
                     }
                 }
