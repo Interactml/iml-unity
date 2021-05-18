aa93a3fc5fde34c6efff733b5e5e5904b16f58ca
ClariceHilton <claricemayhilton@gmail.com>
2021-05-18 11:30:36 +0100

    Change modifiers for IInputType

--- Assets/InteractML/Scripts/IML Nodes/Controller Customisers/IInputType.cs
+++ Assets/InteractML/Scripts/IML Nodes/Controller Customisers/IInputType.cs
@@ -7,17 +7,17 @@ namespace InteractML.ControllerCustomisers
 {
     public interface IInputType
     {
-        public string inputName
+        string inputName
         {
             get;
             set;
         }
 
-        public InputHandler InitializeButtonHandler(int buttonNo, IMLTriggerTypes triggerType, string buttonName);
-        public void LoadDeviceInfo();
-        public void SaveDeviceInfo();
-        public void OnTriggerChange(string handlerName, IMLTriggerTypes triggerT, List<InputHandler> handlers);
-        public void OnButtonChange(string handlerName, int button, List<InputHandler> handlers);
+        InputHandler InitializeButtonHandler(int buttonNo, IMLTriggerTypes triggerType, string buttonName);
+        void LoadDeviceInfo();
+        void SaveDeviceInfo();
+        void OnTriggerChange(string handlerName, IMLTriggerTypes triggerT, List<InputHandler> handlers);
+        void OnButtonChange(string handlerName, int button, List<InputHandler> handlers);
     }
 }
 
