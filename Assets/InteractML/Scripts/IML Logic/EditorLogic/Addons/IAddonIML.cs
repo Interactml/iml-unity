1090b391802f91c8a013e6032d64d9c364c81043
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2022-03-31 20:41:44 +0100

    Defined interface for addons

---
+++ Assets/InteractML/Scripts/IML Logic/EditorLogic/Addons/IAddonIML.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace InteractML.Addons
+{
+    /// <summary>
+    /// Interface that specifies what public methods addons should expose for external consumption
+    /// </summary>
+    public interface IAddonIML 
+    {
+        /// <summary>
+        /// Is addon instance initialized?
+        /// </summary>
+        /// <returns></returns>
+        bool IsInit();
+
+        /// <summary>
+        /// Initializes addon
+        /// </summary>
+        void Initialize();
+
+        /// <summary>
+        /// Adds the addon component to a gameObject (i.e. the IMLComponent)
+        /// </summary>
+        /// <param name="GO"></param>
+        void AddAddonToGameObject(GameObject GO);
+
+        #region Consumed by IMLEditorManager
+
+        // The following are methods consumed by the IMLEditorManager
+        void EditorUpdateLogic();
+        void EditorSceneOpened();
+        void EditorEnteredPlayMode();
+        void EditorEnteredEditMode();
+        void EditorExitingPlayMode();
+        void EditorExitingEditMode();
+
+        #endregion
+
+    }
+}
