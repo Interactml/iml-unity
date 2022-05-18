0aa530a6150b14049098b61843e76fa8cbc92a70
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2022-05-18 20:24:43 +0100

    Creataed RadialMenuManager to handle opening/closing menu in VR

--- Assets/InteractMLVR/RadialMenu/Scripts/RadialMenuManager.cs
+++ Assets/InteractMLVR/RadialMenu/Scripts/RadialMenuManager.cs
@@ -1,8 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class RadialMenuManager : MonoBehaviour
+namespace InteractML.VR
 {
-    
+    /// <summary>
+    /// Manages open/close of radial menu (for the moment)
+    /// </summary>
+    public class RadialMenuManager : MonoBehaviour
+    {
+
+        public RadialMenu radialMenu;
+        public IMLComponent MLSystem;
+
+        public InputActionReference ToggleMenuOpen;
+
+        private void Awake()
+        {
+            if (ToggleMenuOpen != null)
+            {
+                ToggleMenuOpen.action.started += OpenMenu;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (ToggleMenuOpen != null)
+            {
+                ToggleMenuOpen.action.started -= OpenMenu;
+            }
+
+        }
+
+        public void OpenMenu(InputAction.CallbackContext context)
+        {
+            Debug.Log("TriggerOpen called!");
+            if (MLSystem != null)
+            {
+                Debug.Log("Calling select graph...");
+                IMLEventDispatcher.selectGraph?.Invoke(MLSystem);
+            }
+        }
+
+
+    }
+
 }
