eca29766c391c534bc998afd4c76a0ba869b6afc
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2022-10-26 19:31:11 +0100

    Started working on a canvas IMLGraph console

---
+++ Assets/InteractMLVR/CanvasGraphConsole/Scripts/IMLGraphConsole.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InteractML
+{
+    /// <summary>
+    /// Canvas console that displays information about a graph status
+    /// </summary>
+    public class IMLGraphConsole : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
